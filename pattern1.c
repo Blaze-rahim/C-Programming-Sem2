7a0a8ef3f48fe807975e33140d92048106957526
AbduRahim <arahim.as@yahoo.com>
2022-04-16 01:08:20 +0530

    a

--- pattern1.c
+++ pattern1.c
@@ -3,9 +3,11 @@ int main(){
     int r;
     printf("Enter the rows");
     scanf("%d",&r);
-    for(int i = 1; i<=r; i++){
-        for(int j = 1; j<=i; j++){
-            printf("%d",j);
+    for(int i = 1; i<=r; i++){//2
+
+        for(int j = 1; j<=i; j++){//2
+            
+            printf("%d",j); //12
         }
         printf("\n");
     }
