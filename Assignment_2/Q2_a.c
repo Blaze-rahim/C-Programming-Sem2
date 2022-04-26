af2d9efe045579fccac5502eab27d9a964026897
AbduRahim <arahim.as@yahoo.com>
2022-04-26 07:21:58 +0530

    a

--- Assignment_2/Q2_a.c
+++ Assignment_2/Q2_a.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
 int main(){
     int c;
+    printf("Enter month in num");
+    scanf("%d", &c);
     switch (c){
-    case 1:
-    case 3:
-    case 5:
-    case 7:
-    case 8:
-    case 10:
-    case 12:
-        printf("31 Days");
-        break;
-    
-    default:
-        break;
-    }
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            printf("31 Days");
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            printf("30 Days");
+            break;
+        
+        case 2:
+            printf("28/29 Days");
+            break;
+        
+        default:
+            printf("Invalid");
+            break;
+        }
     return 0;
 }
--- Assignment_2/Q3_a.c
+++ Assignment_2/Q3_a.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-    int n1, n2, gdc, lcm, c=1,s;
+    int n1, n2, gcd, lcm, c=1,s;
     printf("2 numbers\n");
     scanf("%d%d", &n1, &n2);
 
@@ -9,13 +9,13 @@ int main(){
     s = n1<n2 ? n1 : n2;
 
     while (c<=s){
-        if(n1 % c == 0 && n2 % c == 0) gdc = c;
+        if(n1 % c == 0 && n2 % c == 0) gcd = c;
         c++;
     }
 
-    lcm  = (n1*n2)/gdc;
+    lcm  = (n1*n2)/gcd;
     
-    printf("GDC is %d and LCM is %d",gdc,lcm);
+    printf("gcd is %d and LCM is %d",gcd,lcm);
 
     return 0;
 }
