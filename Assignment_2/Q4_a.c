94bb6cbba169f820ab0bf01d4739cc2d8ffa045c
AbduRahim <arahim.as@yahoo.com>
2022-05-02 02:19:29 +0530

    a

--- Assignment_2/Q4_a.c
+++ Assignment_2/Q4_a.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 int main(){
-    int n, res = 1;
-    float sum = 0;
+    int n;
+    float sum = 1, res = 1;
     printf("ENter ur n");
     scanf("%d",&n);
     for(int i = 1; i<=n ; i++){
-        for (int j=2; j<=n; j++) res *= j;
-        
+        for (int j=1; j<=i; j++) res *= j;
         sum += 1.0/res;
     }
-    printf(" The sum is : %.3f",sum);
+    printf(" The sum is : %d",sum);
     return 0;
 }
